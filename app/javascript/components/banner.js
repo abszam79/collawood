8e4c4f911bcc318b77bd7b697bd04ebbeb18a282
Amael Béghin <abeghin@gmail.com>
2018-01-08 14:42:13 +0100

    LP front almost over

---
+++ app/javascript/components/banner.js
@@ -0,0 +1,22 @@
+import Typed from 'typed.js';
+
+function loadDynamicBannerText() {
+  const bannerText = document.getElementById('banner-typed-text');
+  if (bannerText) {
+    new Typed('#banner-typed-text', {
+      strings: [
+      "The new Digital Agency in Paris ...",
+      "Just for you ...",
+      "SnowBot",
+      "Tracks the weather",
+      "Finds the best ski resort",
+      "Finds a confortable car",
+      "Finds a warmful apartment",
+      "Finds the right package!"],
+      typeSpeed: 200,
+      loop: true
+    });
+  }
+}
+
+export { loadDynamicBannerText };
