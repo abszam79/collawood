21d205f262f4b5d69d9c61a22a4b52797d9fdcee
anthony hamou <anthonyhamou@yahoo.fr>
2018-01-11 14:46:49 +0100

    comment import bootstrap

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,6 @@
 // layout file, like app/views/layouts/application.html.erb
 
 //console.log('Hello World from Webpacker')
-import "bootstrap";
+// import "bootstrap";
 import { loadDynamicBannerText } from '../components/banner';
 loadDynamicBannerText();
