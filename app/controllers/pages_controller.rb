d08c77cc88d4006aa1180c4e7cba0ade2637aabf
Amael Béghin <abeghin@gmail.com>
2018-01-27 19:49:57 +0100

    tuning js modal and slideshow and chatbot demo gif png

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:home, :about, :templates]
+  skip_before_action :authenticate_user!, only: [:home, :about, :templates, :work]
 
   def home
   end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   get 'about', to: 'pages#about'
   get 'templates', to: 'pages#templates'
+  get 'work', to: 'pages#work'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
